e6be77dd2056475472e75456e1fa5566fe3b620c
K.G <2060713822@qq.com>
2018-02-12 19:12:18 +0800

    add Ditf sprider and save date to file

--- Ditf/Ditf/items.py
+++ Ditf/Ditf/items.py
@@ -3,12 +3,13 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
-class DarlinginthefrankxxItem(scrapy.Item):
+class DitfItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    topic = scrapy.Field()
